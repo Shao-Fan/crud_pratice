00c2bf0c57a3a663063d531858977cd33e9166f5
YOU0036\Bill <you00360842@gmail.com>
2016-09-26 00:42:38 +0800

    留言板做些基本表格及按鈕選取  只有樣子  無法執行

--- resources/views/MessageBoard.blade.php
+++ resources/views/MessageBoard.blade.php
@@ -4,7 +4,29 @@
 
 <h1 style=color:blue> MessageBoard</h1>
 
+<form action="action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="Mickey">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse">
+  <br>
+<!--  Choice Gender-->
+
+  <input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+  <input type="radio" name="gender" value="other"> Other
+  <br>
+  <input type="submit" value="Submit">
+</form>
+
 <p></p>
+<form action="action_page.php">
+  <textarea name="message" rows="10" cols="100">Unfinish.</textarea>
+  <br>
+  <button type="button" onclick="alert('Unfinish!')">up!</button>
+
+</form>
 
 </body>
 </html>
