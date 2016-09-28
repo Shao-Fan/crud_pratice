f15f5c28575173fad2fbbfc1cbbede42b5918462
g9308370 <g9308370@hotmail.com>
2016-09-28 16:31:42 +0800

    create Seeder

---
+++ database/seeds/ArticleTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Article;
+
+
+class ArticleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (range(1,10) as $index) {
+            $article = new Article();
+            $article->title = 'title'.$index;
+            $article->content = 'content'.$index;
+            $article->save();
+        }
+    }
+}
